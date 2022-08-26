6a9e5bf7f8ea14eb5390affa83357255e55579cc
Pakaporn24 <110577837+Pakaporn24@users.noreply.github.com>
2022-08-26 14:54:53 +0700

    Part3-CHALLENGE

--- components/Forecast.js
+++ components/Forecast.js
@@ -1,17 +1,44 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text , StyleSheet } from "react-native";
 
 
  export default function Forecast(props) {
      return (
          <View>
-             <Text>{props.main}</Text>
-             <Text>{props.description}</Text>
-             <View>
-                 <Text>{props.temp}</Text>
-                 <Text>°C</Text>
+            <Text style={style.Text}>main</Text>
+             <Text style={style.Text}>{props.main}</Text>
+             <Text style={style.Text}>description</Text>
+             <Text style={style.Text}>{props.description}</Text>
+             <View style= {{
+                 flexDirection: "row",
+                 justifyContent: 'center',
+
+             }}>
+                 <Text style={style.temp}>{props.temp}</Text>
+                    <Text></Text>
+                 <Text style={style.Text}>  °C</Text>
              </View>
-             
          </View> 
      );
  } 
+
+ const style = StyleSheet.create(
+    {
+        Text: {
+            textAlign: 'center',
+            fontSize: 30,
+            fontWeight: "bold",
+            color: 'white',
+
+        },
+
+        temp: {
+            fontSize: 40, 
+            fontWeight: "bold", 
+            color: 'white', 
+            textAlign: 'center',
+            lineHeight: 40
+
+        }
+    }
+) 
--- components/Weather.js
+++ components/Weather.js
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
 import { ImageBackground, Text, StyleSheet } from 'react-native';
 import Forecast from "./Forecast";
-
+import { View } from "react-native";
+import Constants from 'expo-constants';
 
  export default function Weather(props) {
      const [forecastInfo, setForecast] = useState(
          {
-             main: '-',
-             description: '-',
-             temp: 0
-         }
-     )
-     return (
-         <ImageBackground source={require('../gg.jpg')} style={style.backdrop}>
-             <Text>Zip Code</Text>
-             <Text>{props.zipCode}</Text>
-             
-             <Forecast {...forecastInfo}/>
+            main: '',
+            description: '',
+            temp: 0
+        }
+    )
+    return (
+        <ImageBackground source={require('../gg.jpg')} style={style.backdrop}>
+               <View style={style.highlight}>
+                 <Text style={style.titleText}>Zip code is {props.zipCode}.</Text>
+                 <Forecast {...forecastInfo}/>
+             </View>
          </ImageBackground>
 
      );
@@ -28,6 +29,21 @@ import Forecast from "./Forecast";
              alignItems: 'center',
              width: '100%',
              height: '100%'
-         }
-     }
- ) 
+         },
+         highlight: {
+            backgroundColor: 'rgba(0, 0, 0, 0.5)',
+            width:"100%", 
+            height:"35%", 
+            paddingTop: Constants.statusBarHeight, 
+            alignItems: 'center'
+        },
+
+        titleText: {
+            fontSize: 25,
+            fontWeight: "bold",
+            color: 'white',
+            textAlign: 'center'
+        }
+    }
+) 
+       
